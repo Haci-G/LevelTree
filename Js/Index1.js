78ed69d304807994dde2ec8e337ca3cfdac7733d
Haci-G <Haci.guney@brightest.be>
2022-02-17 13:06:50 +0100

    update project seasons

--- Js/Index1.js
+++ Js/Index1.js
@@ -31,7 +31,7 @@ window.addEventListener('load', (event) => {
     setTimeout(terugNaarStart, 500);*/
 
     //Functie om achtergrond aan te passen naar gelang het seizoen
-    CheckSeizoen();
+    // CheckSeizoen();
 
     /*Fix voor probleem met de uilijning van Brightest text in de modal bij gebruik van Safari*/
     if (browserChecker("safari")) {
@@ -1185,61 +1185,92 @@ const DataLevels = [
 
 CreateToolTips();
 
-//Seizoen animatie
-const canvas = document.getElementById("test"),
-    ctx = canvas.getContext("2d"),
-    stack = [],
-    w = window.innerWidth,
-    h = window.innerHeight;
 
-const drawer = function () {
-    ctx.fillStyle = "#ffffff";
-    ctx.fillRect(0, 0, w, h);
-    stack.forEach(function (el) {
-        el();
-    });
-    requestAnimationFrame(drawer);
-};
 
-const anim = function () {
-    let x = 0;
-    const speed = Math.random() * 0.7;
-    const position = Math.random() * w - w / 2;
-    const maxTall = Math.random() * 70;
-    const maxSize = Math.random() * 8;
-    const c = function (l, u) {
-        return Math.round(Math.random() * (u || 255) + l || 0);
-    };
-    const color = "rgb(" + c(60, 10) + "," + c(201, 50) + "," + c(120, 50) + ")";
+//Season Switch
+function SeasonSwitch(obj) {
 
-    return function () {
-        const deviation = Math.cos(x / 30) * Math.min(x / 40, 50),
-            tall = Math.min(x / 5, maxTall),
-            size = Math.min(x / 50, maxSize);
 
-        x += speed;
-        ctx.save();
 
-        ctx.strokeWidth = 10;
-        ctx.translate(w / 2 + position, h);
-        ctx.fillStyle = color;
-
-        ctx.beginPath();
-        ctx.lineTo(-size, 0);
-        ctx.quadraticCurveTo(-size, -tall / 2, deviation, -tall);
-        ctx.quadraticCurveTo(size, -tall / 2, size, 0);
-        //ctx.closePath();
-        ctx.fill();
-
-        ctx.restore();
-    };
-};
-
-for (var x = 0; x < 300; x++) {
-    stack.push(anim());
+    console.log(obj.checked)
+    //
+    switch (obj.getAttribute("data-season")) {
+        case "Spring":
+            obj.checked == false ? document.getElementById("spring").style.display = "none" : document.getElementById("spring").style.display = "block";
+            break;
+        case "Summer":
+            // Need to add sun effect behind the righte upper icon.
+            break;
+        case "Winter":
+            obj.checked == false ? (snowStorm.freeze() || snowStorm.stop()) : (snowStorm.resume() || snowStorm.start());
+            break;
+        case "Autumn":
+            // Missing Autumn effect.
+            break;
+    }
 }
 
-canvas.width = w;
-canvas.height = h;
+//Seizoen animatie
 
-drawer();
+SpringSeason()
+
+function SpringSeason() {
+    const canvas = document.getElementById("spring"),
+        ctx = canvas.getContext("2d"),
+        stack = [],
+        w = window.innerWidth,
+        h = window.innerHeight;
+
+    const drawer = function () {
+        ctx.fillStyle = "#ffffff";
+        ctx.fillRect(0, 0, w, h);
+        stack.forEach(function (el) {
+            el();
+        });
+        requestAnimationFrame(drawer);
+    };
+
+    const anim = function () {
+        let x = 0;
+        const speed = Math.random() * 0.7;
+        const position = Math.random() * w - w / 2;
+        const maxTall = Math.random() * 70;
+        const maxSize = Math.random() * 8;
+        const c = function (l, u) {
+            return Math.round(Math.random() * (u || 255) + l || 0);
+        };
+        const color = "rgb(" + c(60, 10) + "," + c(201, 50) + "," + c(120, 50) + ")";
+
+        return function () {
+            const deviation = Math.cos(x / 30) * Math.min(x / 40, 50),
+                tall = Math.min(x / 5, maxTall),
+                size = Math.min(x / 50, maxSize);
+
+            x += speed;
+            ctx.save();
+
+            ctx.strokeWidth = 10;
+            ctx.translate(w / 2 + position, h);
+            ctx.fillStyle = color;
+
+            ctx.beginPath();
+            ctx.lineTo(-size, 0);
+            ctx.quadraticCurveTo(-size, -tall / 2, deviation, -tall);
+            ctx.quadraticCurveTo(size, -tall / 2, size, 0);
+            //ctx.closePath();
+            ctx.fill();
+
+            ctx.restore();
+        };
+    };
+
+    for (var x = 0; x < 300; x++) {
+        stack.push(anim());
+    }
+
+    canvas.width = w;
+    canvas.height = h;
+
+    drawer();
+
+}
